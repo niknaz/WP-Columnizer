76c44b1049ba2c74bfa565080034ed1a0efc501d
Tristan Chambers <tristan.chambers@gmail.com>
2012-08-10 15:04:00 -0400

    First Commit

---
+++ columnizer-init.js
@@ -0,0 +1,16 @@
+jQuery(
+	function(){
+		var divwidth = parseInt(jQuery('#main').css('width'));
+		jQuery('.columnize-two').columnize({width:divwidth/2});
+		jQuery('.columnize-three').columnize({width:divwidth/3});
+		jQuery('.columnize-four').columnize({width:divwidth/4});
+		jQuery('.columnize-five').columnize({width:divwidth/5});
+		jQuery('.columnize-six').columnize({width:divwidth/6});
+		jQuery('.columnize-seven').columnize({width:divwidth/7});
+		jQuery('.columnize-eight').columnize({width:divwidth/8});
+		jQuery('.columnize-nine').columnize({width:divwidth/9});
+		jQuery('.columnize-ten').columnize({width:divwidth/10});
+		jQuery('.columnize-eleven').columnize({width:divwidth/11});
+		jQuery('.columnize-twelve').columnize({width:divwidth/12});
+	}
+);
---
+++ columnizer.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: Columnizer
+Description: Automatically formats content into columns
+Version: 3.3.1.150
+Author: Tristan Chambers tristan.chambers@gmail.com
+*/
+
+// Use this for testing - comment out for release
+error_reporting(E_ALL);
+
+function columnizer_scripts_method() {
+        wp_enqueue_script('jquery');
+
+        wp_register_script('columnizer', plugins_url('jquery.columnizer.js', __FILE__));
+        wp_enqueue_script('columnizer');
+        wp_register_script('columnizerinit', plugins_url('columnizer-init.js', __FILE__));
+        wp_enqueue_script('columnizerinit');
+
+        wp_register_style('columnizer', plugins_url('columnizer.css', __FILE__));
+        wp_enqueue_style('columnizer');
+}
+
+add_action('wp_enqueue_scripts', 'columnizer_scripts_method');
+
+//[twocolumns]
+function columnizer_two_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-two">' . $content . '</div>';
+}
+add_shortcode( 'twocolumns', 'columnizer_two_shortcode' );
+//[threecolumns]
+function columnizer_three_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-three">' . $content . '</div>';
+}
+add_shortcode( 'threecolumns', 'columnizer_three_shortcode' );
+//[fourcolumns]
+function columnizer_four_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-four">' . $content . '</div>';
+}
+add_shortcode( 'fourcolumns', 'columnizer_four_shortcode' );
+//[fivecolumns]
+function columnizer_five_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-five">' . $content . '</div>';
+}
+add_shortcode( 'fivecolumns', 'columnizer_five_shortcode' );
+//[sixcolumns]
+function columnizer_six_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-six">' . $content . '</div>';
+}
+add_shortcode( 'sixcolumns', 'columnizer_six_shortcode' );
+//[sevencolumns]
+function columnizer_seven_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-seven">' . $content . '</div>';
+}
+add_shortcode( 'sevencolumns', 'columnizer_seven_shortcode' );
+//[eightcolumns]
+function columnizer_eight_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-eight">' . $content . '</div>';
+}
+add_shortcode( 'eightcolumns', 'columnizer_eight_shortcode' );
+//[ninecolumns]
+function columnizer_nine_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-nine">' . $content . '</div>';
+}
+add_shortcode( 'ninecolumns', 'columnizer_nine_shortcode' );
+//[tencolumns]
+function columnizer_ten_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-ten">' . $content . '</div>';
+}
+add_shortcode( 'tencolumns', 'columnizer_ten_shortcode' );
+//[elevencolumns]
+function columnizer_eleven_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-eleven">' . $content . '</div>';
+}
+add_shortcode( 'elevencolumns', 'columnizer_eleven_shortcode' );
+//[twelvecolumns]
+function columnizer_twelve_shortcode( $atts, $content = null ) {
+   return '<div class="columnizer columnize-twelve">' . $content . '</div>';
+}
+add_shortcode( 'twelvecolumns', 'columnizer_twelve_shortcode' );
+
